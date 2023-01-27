0a1a6521a013d379932ba05909ba6d0c2e53df54
Julia3099 <121186506+Julia3099@users.noreply.github.com>
2023-01-27 22:43:20 +0500

    Add files via upload

---
+++ 2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("Введите первое число: ");
+String number1 = Console.ReadLine();
+int a = Convert.ToInt32(number1);
+System.Console.Write("Введите второе число: ");
+String number2 = Console.ReadLine();
+int b = Convert.ToInt32(number2);
+System.Console.Write("Введите третье число: ");
+String number3 = Console.ReadLine();
+int c = Convert.ToInt32(number3);
+int max = a;
+if (b > max)
+{
+    max = b;
+    if (c > max)
+    {
+        Console.Write($"Число {c} максимальное");
+    }
+    else Console.Write($"Число {b} максимальное");
+}
+else if (c > max)
+{
+    max = c;
+    Console.Write($"Число {c} максимальное");
+}
+else Console.Write($"Число {a} максимальное");
---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Введите любое натуральное число > ");
+String number1 = Console.ReadLine();
+int a = Convert.ToInt32(number1);
+int index = 1;
+while (index <= a)
+{
+    if (index % 2 == 0)
+    {
+        System.Console.Write($"{index} ");
+    }
+    index++;
+}

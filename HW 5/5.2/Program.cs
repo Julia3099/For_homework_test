0c0c68019cb2fa0cb554e83ae3789d0d83f1e0a9
Julia3099 <121186506+Julia3099@users.noreply.github.com>
2023-02-09 23:00:25 +0500

    Add files via upload

---
+++ HW 5/5.2/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[] GenerateArray(int digit)
+{
+    int[] array = new int[digit];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-50, 50);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item} \t");
+    }
+    System.Console.WriteLine();
+}
+
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        count += array[i];
+    }
+    return count;
+    }
+
+
+int digit = Prompt("Введите число");
+int[] array = GenerateArray(digit);
+PrintArray(array);
+System.Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях, равна > {Count(array)}");
---
+++ HW 5/5.3/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Задача 3: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+double[] GenerateArray(int len)
+{
+    double[] array = new double[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        double num = Math.Round(rnd.NextDouble() * (1000 - 1), 1);
+        array[i] = num;
+    }
+    return array;
+}
+
+void Printarray(double[] array)
+{
+    foreach (var item in array)
+    {
+        System.Console.Write($"{item} \t");
+    }
+    System.Console.WriteLine();
+}
+
+double Max(double []array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+        
+    }
+    return max;
+}
+
+double Min(double []array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+        
+    }
+    return min;
+}
+
+int digit = Prompt("Введите число");
+double[] array = GenerateArray(digit);
+double max = Max(array);
+double min = Min(array);
+Printarray(array);
+System.Console.WriteLine($"Разница между максимальным {max} и минимальным {min} составляет > {Math.Round(max - min, 2)}");

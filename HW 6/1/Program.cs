0914710fdce69531c6280eb26bb457ca83b7b9c2
Julia3099 <121186506+Julia3099@users.noreply.github.com>
2023-02-19 15:50:44 +0500

    Add files via upload

---
+++ HW 6/1/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 1: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int InputInt(string message)
+{
+    System.Console.Write($"{message}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int [] FillArray(int len)
+{
+    int [] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"Введите {i + 1}е число > ");
+        array[i] = int.Parse(System.Console.ReadLine());
+    }
+    return array;
+}
+
+int Count(int [] num)
+{
+    int count = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        if (num[i] > 0) count++;
+    }
+    return count;
+}
+
+void ShowArray(int [] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.WriteLine($"{item}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int num = InputInt("Какое количество чисел хотите ввести? > ");
+int []array = FillArray(num);
+ShowArray(array);
+int count = Count(array);
+System.Console.Write($"Чисел больше 0 введено > {count}");
---
+++ HW 6/2/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+int InputInt(string message)
+{
+    System.Console.Write($"{message}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+double IntersectionX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double IntersectionY(double b1, double k1, double b2, double k2)
+{
+    double y = k1 * IntersectionX(b1, k1, b2, k2) + b1;
+    return y;
+}
+
+bool Parallel(double k1, double k2)
+{
+    if (k1 == k2)
+    {
+        System.Console.WriteLine("Точки пересечения нет. Прямые параллельны либо совпадают");
+        return false;
+    }
+    else return true;
+}
+
+System.Console.WriteLine($"Уравнения прямой выглядит следующим образом > y = k * x + b");
+
+int b1 = InputInt("Введите b1 > ");
+int k1 = InputInt("Введите k1 > ");
+int b2 = InputInt("Введите b2 > ");
+int k2 = InputInt("Введите k2 > ");
+
+if (Parallel(k1, k2))
+{
+    double x = IntersectionX(b1, k1, b2, k2);
+    double y = IntersectionY(b1, k1, b2, k2);
+    System.Console.WriteLine($"Точка пересечения с координатами: > ({x}, {y})");
+}
+

44f7ed785d73450ccbf695e81d8660b619028f84
Julia3099 <121186506+Julia3099@users.noreply.github.com>
2023-02-21 22:49:48 +0500

    Add files via upload

---
+++ HW 7/2/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+Задача 2. Напишите программу, которая на вход принимает 
+позиции элемента в двумерном массиве, и возвращает значение 
+этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1, 7 -> такого числа в массиве нет
+1, 1 -> 1
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+
+bool ValidatePosition(int[,] array, int x, int y)
+{
+    if (x > array.GetLength(0) || y > array.GetLength(1))
+    {
+        System.Console.WriteLine("Элемента с такой позицией нет. Попробуйте ещё раз.");
+        return false;
+    }
+    return true;
+}
+
+int PositionOfElement(int[,] array, int x, int y)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == x -1 && j == y - 1) return array[i, j];
+        }
+    }
+    return array[x, y];
+}
+
+int[,] matrix = new int [5, 5];
+FillArray(matrix);
+PrintArray(matrix);
+int firstPosition = Prompt("Введите номер строки элемента ");
+int secondPosition = Prompt("Введите номер столбца элемента ");
+if (ValidatePosition(matrix, firstPosition, secondPosition))
+{
+    int pos = PositionOfElement(matrix, firstPosition, secondPosition);
+    System.Console.WriteLine($"На данной позиции находится элемент > {pos}");
+}
---
+++ HW 7/3/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Задача 3. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int row = array.GetLength(0);
+    int col = array.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double AverageOfColumns(int[,] array)
+{
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        System.Console.WriteLine($"sum / array.GetLength(1)");
+    }
+}
+
+int[,] array = new int[5, 5];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine($"Среднее арифметическое элементов по столбцам > {AverageOfColumns(array)}");
